3d0078372f9218585e9a86c1d35b405e257a2539
Ali Amjad <66186390+Ali-A-Koye@users.noreply.github.com>
2021-02-11 22:29:26 +0300

    Update main.jsx

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from 'react';
 import Header from './Header';
 import Chart from './chart.jsx'
 import './main.css'
@@ -8,16 +8,14 @@ function Main(props) {
 
     console.log(props.HeaderPart);
 
-    let background= 'background-day'
-    if(props.HeaderPart.current.is_day === 'no'){
-        background ='background-night'
+    let background = 'background-day'
+    if (props.HeaderPart.current.is_day === 'no') {
+        background = 'background-night'
     }
 
     return (
         <div class={background}>
-            <Header data={props.HeaderPart}/>
-
-        <div>
+            <Header data={props.HeaderPart} />
             <Chart data={props.chartData} />
         </div>
     );
